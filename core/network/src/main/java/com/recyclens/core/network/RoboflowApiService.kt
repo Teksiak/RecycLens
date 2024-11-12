ac04ceb3a457010f74cb4a828e783e6d096ed3e5
taqix <taqiedits@gmail.com>
2024-11-12 21:58:56 +0100

    fix

--- core/network/src/main/java/com/recyclens/core/network/RoboflowApiService.kt
+++ core/network/src/main/java/com/recyclens/core/network/RoboflowApiService.kt
@@ -1,16 +1,11 @@
 package com.recyclens.core.network
 
 import com.recyclens.core.network.dto.PredictionResultDto
-import dagger.hilt.android.AndroidEntryPoint
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
-import retrofit2.http.Headers
 import retrofit2.http.POST
-import kotlin.io.encoding.Base64
 
-@AndroidEntryPoint
 interface RoboflowApiService {
     @FormUrlEncoded
     @POST("/recycling-classification-ajsxm/2?api_key=${BuildConfig.API_KEY}")
