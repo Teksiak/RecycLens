28d013c0c00ab36f24b0fab5d70112cf1899d396
taqix <taqiedits@gmail.com>
2024-11-12 12:50:05 +0100

    feat: new colors added

--- core/presentation/src/main/java/com/recyclens/core/presentation/designsystem/Color.kt
+++ core/presentation/src/main/java/com/recyclens/core/presentation/designsystem/Color.kt
@@ -2,10 +2,29 @@ package com.recyclens.core.presentation.designsystem
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Primary = Color(0xFF31511E)
+val Secondary = Color(0xFF859F3D)
+val Tetriary = Color(0xFFF6FCDF)
+val Dark = Color(0xFF1A1A19)
+val White = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val PaperDark = Color(0xFF003C90)
+val PaperMain = Color(0xFF0F52BA)
+
+val PlasticDark = Color(0xFFDAA520)
+val PlasticMain = Color(0xFFFFBF00)
+
+val MixedDark = Color(0xFF2A2929)
+val MixedMain = Color(0xFF55555B)
+
+val GlassDark = Color(0xFF1B691B)
+val GlassMain = Color(0xFF228B22)
+
+val BioDark = Color(0xFF493121)
+val BioMain = Color(0xFF5C4033)
+
+val ElectronicsDark = Color(0xFF5F1313)
+val ElectronicsMain = Color(0xFF7B1818)
+
+val ErrorDark = Color(0xFF94001D)
+val ErrorMain = Color(0xFFE8002E)
