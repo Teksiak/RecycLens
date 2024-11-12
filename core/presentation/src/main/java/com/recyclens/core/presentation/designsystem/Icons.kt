9117203eabd94a34c9ceb11eaacac005c3378765
taqix <taqiedits@gmail.com>
2024-11-12 13:34:45 +0100

    feat: added icons

---
+++ core/presentation/src/main/java/com/recyclens/core/presentation/designsystem/Icons.kt
@@ -0,0 +1,57 @@
+package com.recyclens.core.presentation.designsystem
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import com.recyclens.core.presentation.R
+val PlasticBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.plastic_bin)
+val PaperBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.paper_bin)
+val BioBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.bio_bin)
+val GlassBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.glass_bin)
+val MixedBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.mixed_bin)
+val Star: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.star)
+val Trash: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.trash)
+val ElectronicBin: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.electronic_bin)
+val Help: ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.pomoc)
+val LogoFirst : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.logo_first_theme)
+val LogoSecond : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.logo_second_theme)
+val Error : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.error)
+val Flash : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.flash)
+val Close : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.close)
+val History : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.history)
+val CheckImage : ImageVector
+    @Composable
+    get() = ImageVector.vectorResource(id = R.drawable.check_image)
+
+
+
