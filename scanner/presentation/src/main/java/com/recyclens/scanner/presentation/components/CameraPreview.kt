c33a8281a38665e28563042999fefec0526fda25
Teksiak <bednarowski.wojciech@gmail.com>
2024-11-13 18:51:17 +0100

    fix: Fix scaling the image when taking a photo.

--- scanner/presentation/src/main/java/com/recyclens/scanner/presentation/components/CameraPreview.kt
+++ scanner/presentation/src/main/java/com/recyclens/scanner/presentation/components/CameraPreview.kt
@@ -5,14 +5,23 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.draw.drawWithContent
 import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.compose.LocalLifecycleOwner
 
@@ -23,40 +32,36 @@ fun CameraPreview(
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
 
-    AndroidView(
+    Box(
         modifier = Modifier
-            .fillMaxSize()
-            .clipToBounds()
-            .then(
-                if(previewImage != null) {
-                    Modifier.drawWithContent {
-                        // TODO: Fix this to draw the image correctly
-                        drawImage(
-                            previewImage,
-                            dstOffset = IntOffset(
-                                0, 0
-                            ),
-                            dstSize = IntSize(
-                                width = size.width.toInt(),
-                                height = size.height.toInt()
-                            ),
-                        )
-                    }
-                } else Modifier
-            )
-
-        ,
-        factory = { viewContext ->
-            PreviewView(viewContext).apply {
-                layoutParams = LinearLayout.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                setBackgroundColor(Color.BLACK)
-                scaleType = PreviewView.ScaleType.FILL_START
-                this.controller = cameraController
-                cameraController.bindToLifecycle(lifecycleOwner)
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize(),
+            factory = { viewContext ->
+                PreviewView(viewContext).apply {
+                    layoutParams = LinearLayout.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    setBackgroundColor(Color.BLACK)
+                    scaleType = PreviewView.ScaleType.FILL_CENTER
+                    this.controller = cameraController
+                    cameraController.bindToLifecycle(lifecycleOwner)
+                }
             }
+        )
+        previewImage?.let {
+            Image(
+                bitmap = it,
+                contentDescription = null,
+                modifier = Modifier
+                    .clipToBounds(),
+                contentScale = ContentScale.Crop,
+                alignment = Alignment.Center
+            )
         }
-    )
+    }
 }
