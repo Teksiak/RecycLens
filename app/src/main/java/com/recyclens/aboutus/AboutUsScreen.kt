085cc9ee9ad090cbf27b17216fe9f6dc8ed81671
Teksiak <bednarowski.wojciech@gmail.com>
2024-11-14 09:18:15 +0100

    fix: Remove imports.

--- app/src/main/java/com/recyclens/aboutus/AboutUsScreen.kt
+++ app/src/main/java/com/recyclens/aboutus/AboutUsScreen.kt
@@ -9,37 +9,25 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.BasicText
-import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.recyclens.R
 import com.recyclens.core.presentation.components.NavigationTopBar
-import com.recyclens.core.presentation.designsystem.LogoFirst
-import com.recyclens.core.presentation.designsystem.LogoSecond
-import com.recyclens.core.presentation.designsystem.PlasticDark
-import com.recyclens.core.presentation.designsystem.PlasticMain
 import com.recyclens.core.presentation.designsystem.Primary
 import com.recyclens.core.presentation.designsystem.RecycLensTheme
 import com.recyclens.core.presentation.designsystem.Secondary
@@ -97,13 +85,11 @@ fun AboutUsScreen() {
                 ),
             )
 
-            // First paragraph
             Text(
                 text = stringResource(id = R.string.watchword),
                 style = MaterialTheme.typography.bodyMedium,
             )
             Spacer(modifier = Modifier.size(48.dp))
-            // Second paragraph
             Text(
                 text = stringResource(id = R.string.about_us_description),
                 style = MaterialTheme.typography.bodyLarge,
@@ -117,12 +103,11 @@ fun AboutUsScreen() {
             )
             Spacer(modifier = Modifier.size(16.dp))
             Text(
-                text =
-                    stringResource(id = R.string.contact_email),
+                text = stringResource(id = R.string.contact_email),
                 style = MaterialTheme.typography.bodyMedium.copy(
-                    color = Primary,
                     textDecoration = TextDecoration.Underline
                 ),
+                color = Primary,
             )
             Spacer(modifier = Modifier.size(16.dp))
             Text(
--- history/domain/build.gradle.kts
+++ history/domain/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("java-library")
-
     alias(libs.plugins.kotlin.jvm)
 }
 
@@ -13,7 +12,4 @@ kotlin {
     compilerOptions {
         jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
     }
-}
-dependencies {
-    implementation(libs.room.compiler)
 }
